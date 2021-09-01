df30f2f489a41377dc2b0bf93cdfe87e1d53d1eb
Parthav Dani <partharjun@gmail.com>
2021-08-31 20:22:52 -0400

    Uploading project

---
+++ src/feature1/Class1.java
@@ -0,0 +1,12 @@
+package feature1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("First set of changes line1 in Class1 for master");
+		System.out.println("First set of changes line2 in Class1 for master");
+		System.out.println("First set of changes line3 in Class1 for master");
+
+	}
+
+}
---
+++ src/feature1/Class2.java
@@ -0,0 +1,12 @@
+package feature1;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("First set of changes line1 in Class2 for master");
+		System.out.println("First set of changes line2 in Class2 for master");
+		System.out.println("First set of changes line3 in Class2 for master");
+
+	}
+
+}
